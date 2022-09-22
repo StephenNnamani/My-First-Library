e727ef07b41dcdda3c46eb33c04c171bf551253f
StephenNnamani <sir.stephennnamani@gmail.com>
2022-09-22 08:21:12 +0100

    22/09/2022 (Morning)

--- FizzBuzz Library/Class1.cs
+++ FizzBuzz Library/Class1.cs
@@ -4,11 +4,38 @@ namespace FizzBuzz_Library
 {
     public class Class1
     {
-        private static FizzBuzz(int number);
+        public static main()
         {
-            string name = "stephen";
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            Console.WriteLine(@"ODD NUMBER GAME \nPick a number from 100 to 1000 \n guess the number of odd numbers that are in counting from 1 to the number you chose!!");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------ \n");
 
-            
+            int guess = Console.ReadLine("Guess a number!!");
+
+            Console.WriteLine(OddNumberGame(guess));
+
+            return guess;
+
+            Console.ReadKey();            
+        }
+        private OddNumberGame(int guess)
+        {
+            int guess;
+
+            do
+            {
+                i = 1;
+                if (guess % 2 != 0)
+                {
+                    guess--;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                    i++;
+                    guess--;
+                }
+            } while (guess != 0);
         }
     }
 }
